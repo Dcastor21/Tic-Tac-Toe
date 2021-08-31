015422c7228aff7da03165cc478b39a242ff87d1
Darnel Castore <dcastor@student.clayton.edu>
2021-08-31 13:40:53 -0400

    add scripts to info.py

--- contents/info.py
+++ contents/info.py
@@ -1,14 +1,28 @@
 import random
 from IPython.display import clear_output
 
-def display_board(board):
+def main():
+   def display_board(board):
 
-   print('   |   | ')
-    print('',board[1],'|',board[2],'|',board[3])
-    print('___|___|___')
-    print('   |   | ')
-    print('',board[4],'|',board[5],'|',board[6])
-    print('___|___|___')
-    print('   |   | ')
-    print('',board[7],'|',board[8],'|',board[9])
-    print('   |   |')
+      print('   |   | ')
+      print('',board[1],'|',board[2],'|',board[3])
+      print('___|___|___')
+      print('   |   | ')
+      print('',board[4],'|',board[5],'|',board[6])
+      print('___|___|___')
+      print('   |   | ')
+      print('',board[7],'|',board[8],'|',board[9])
+      print('   |   |')
+
+   def player_input():
+      pass
+   
+   def place_maker(board, marker, position):
+   
+      pass
+
+   def win_check(board,mark):
+      pass
+   def space_check(board, position):
+        
+      pass
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-from contents.info import 
+from contents import info
 
+def main():
+    print("Welcome to Tic Tac Toe\n----------------------\n")
 
+if __name__=='__main__':
+    main()
+    info.main()
